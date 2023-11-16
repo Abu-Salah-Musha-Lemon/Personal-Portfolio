492d68f611557036814ff850c84f60f23f3686ca
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-11-16 20:45:00 +0600

    update

--- script/aciveLInk.js
+++ script/aciveLInk.js
@@ -12,3 +12,18 @@ const navLinks = document.querySelectorAll('.navbar a').forEach(link => {
 //   console.log(pageName);
 //   document.querySelectorAll('.hom').classList.replace('hom','active');
 // }
+
+// $(document).ready(function(){
+//   $('navbar a').click(function(){
+//     $('navbar a').removeClass("active");
+//     $(this).addClass("active");
+// });
+// });
+{/* <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script> */}
+
+
+let select = window.location.pathname.split('/')[1];
+let navbar = document.querySelectorAll('navbar').forEach(link=>{
+   let active = link.classList.add('active');
+  console.log(active);
+})
